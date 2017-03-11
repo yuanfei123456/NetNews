d2475f602160b39c4cc7a2d9d13d129b0067a1dc
YuanFei <479750400@qq.com>
2017-03-11 21:47:36 +0800

    点击Channel上的Label使其滑动到中间位置 点击Channel上的Label使newsView滑动到对应位置

---
+++ NetNews/NetNews/Clesses/Home/View/ChannelLabel.h
@@ -0,0 +1,14 @@
+//
+//  ChannelLabel.h
+//  NetNews
+//
+//  Created by 袁飞 on 2017/3/11.
+//  Copyright © 2017年 Yuanfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChannelLabel : UILabel
+
+
+@end
---
+++ NetNews/NetNews/Clesses/Home/View/ChannelLabel.m
@@ -0,0 +1,21 @@
+//
+//  ChannelLabel.m
+//  NetNews
+//
+//  Created by 袁飞 on 2017/3/11.
+//  Copyright © 2017年 Yuanfei. All rights reserved.
+//
+
+#import "ChannelLabel.h"
+
+@implementation ChannelLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
