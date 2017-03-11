a0ae7fae5d2c10132bc64f5660cd344eab042e74
YuanFei <479750400@qq.com>
2017-03-11 20:57:30 +0800

    newsShowTable正确显示

---
+++ NetNews/NetNews/Clesses/Home/Controller/ImagesModel.h
@@ -0,0 +1,15 @@
+//
+//  ImagesModel.h
+//  NetNews
+//
+//  Created by 袁飞 on 2017/3/11.
+//  Copyright © 2017年 Yuanfei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImagesModel : NSObject
+
+@property (nonatomic, copy) NSString *imgsrc;
+
+@end
---
+++ NetNews/NetNews/Clesses/Home/Controller/ImagesModel.m
@@ -0,0 +1,13 @@
+//
+//  ImagesModel.m
+//  NetNews
+//
+//  Created by 袁飞 on 2017/3/11.
+//  Copyright © 2017年 Yuanfei. All rights reserved.
+//
+
+#import "ImagesModel.h"
+
+@implementation ImagesModel
+
+@end
